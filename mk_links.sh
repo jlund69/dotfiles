cc962d3c1747c355aa64798971531d42def18395
John <jlund69@users.noreply.github.com>
2022-04-07 15:14:55 -0400

    add script to copy scripts

---
+++ mk_links.sh
@@ -0,0 +1,10 @@
+ln ./.ssh/config ~/.ssh/config
+ln ./etc/ansible/hosts ~/etc/ansible/hosts
+ln -s ./.scripts ~/.scripts
+ln ./.ansible.cfg ~/.ansible.cfg
+ln ./.bashrc ~/.bashrc
+ln ./.bash_profile ~/.bash_profile
+ln ./.autoenv_authorized ~/.autoenv_authorized
+ln ./.gitignore_global ~/.gitignore_global
+ln ./.mysql.cnf ~/.mysql.cnf
+ln -s ./Brewfile ~/Brewfile
