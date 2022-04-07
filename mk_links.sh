fec2ec10ecb7af4668ced6e15135fdf26e072ed8
John <jlund69@users.noreply.github.com>
2022-04-07 15:17:53 -0400

    fix errors

--- mk_links.sh
+++ mk_links.sh
@@ -1,4 +1,5 @@
 ln ./.ssh/config ~/.ssh/config
+mkdir ~/etc/ansible
 ln ./etc/ansible/hosts ~/etc/ansible/hosts
 ln -s ./.scripts ~/.scripts
 ln ./.ansible.cfg ~/.ansible.cfg
@@ -6,5 +7,5 @@ ln ./.bashrc ~/.bashrc
 ln ./.bash_profile ~/.bash_profile
 ln ./.autoenv_authorized ~/.autoenv_authorized
 ln ./.gitignore_global ~/.gitignore_global
-ln ./.mysql.cnf ~/.mysql.cnf
+ln ./mysql.cnf ~/mysql.cnf
 ln -s ./Brewfile ~/Brewfile
